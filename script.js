a1b669ae63d0944d8ba32af139e7b2faa2a52e8b
Gold3n-LCP <lukepalassis@gmail.com>
2024-03-24 16:00:05 -0400

    problem with app.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const getCrafts = async () => {
+  try {
+    return (await fetch("api/crafts/")).json();
+    console.log("api/crafts/").json();
+    console.log("JSON CONTENTS");
+  } catch (error) {
+    console.log(errpr);
+  }
+};
 
+const showCrafts = async () => {
+  let crafts = await getCrafts();
+  const craftList = document.getElementById("char-list");
+  craftList.innerHTML = "";
+  crafts.forEach((craft) => {
+    const craftSection = craft.sect;
+    craftSection.craft = craft;
+    craftList.append(craftSection);
+  });
+};
+
+//https://www.w3schools.com/w3css/w3css_modal.asp
+document.querySelectorAll("section").forEach((section) => {
+  section.onclick = (e) => {
+    const craft = e.currentTarget.craft;
+    const dialog = document.getElementById("dialog");
+
+    dialog.style.display = "block";
+
+    document.querySelector("#dialog-content img").src =
+      e.currentTarget.querySelector("img").src;
+
+    const dialogDetails = document.querySelector("#dialog-details");
+
+    dialogDetails.innerHTML = "";
+    dialogDetails.append(craft.expandedSect);
+  };
+});
